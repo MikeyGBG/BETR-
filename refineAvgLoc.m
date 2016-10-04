19f661fa32f25d7a23e5dbee05d986eed0300032
MikeyGBG <Michael.Garthe@hotmail.com>
2016-10-04 10:31:09 +1000

    Update refineAvgLoc.m

--- refineAvgLoc.m
+++ refineAvgLoc.m
@@ -1,11 +1,12 @@
 function avgLoc = refineAvgLoc(averageLocation, clumps)
+%Removes the outliers from the clumps which are further from the average
+%location by the distance threshold
 locations = [];
-distThresh = 100;
+distThresh = 100;   %Distance threshold
 l = 1;
-
-%if averageLocation == [];
-%    averageLocation = [20,20];
-
+    
+%Find the distance from clump to average location, adding the clump to the
+%locations list if it is within the distancer threshold away
     for i = 1:length(clumps)
         try
             if ~(sqrt(((clumps(i,1) - averageLocation(1))^2 + (clumps(i, 2) - averageLocation(2))^2)) > distThresh)
@@ -17,5 +18,7 @@ l = 1;
         end
     
     end
+    %Find the new average location of the clumps after the outliers have
+    %been removed
     avgLoc = getAverageLocation(locations);
 end
