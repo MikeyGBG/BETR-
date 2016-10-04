9970152039b89ec7860f81d26162f9ab600114aa
MikeyGBG <Michael.Garthe@hotmail.com>
2016-10-04 10:30:45 +1000

    Update getAverageLocation.m

--- getAverageLocation.m
+++ getAverageLocation.m
@@ -1,10 +1,13 @@
+
 function averageLocation = getAverageLocation(clumps)
+%Finds the average location of all the clumps in the scene
 
 locationSumx = 0;
 locationSumy = 0;
 
     for i = 1:length(clumps)
         try
+            %Add the x and y distances between every clump
             locationSumx = locationSumx + clumps(i,1);
             locationSumy = locationSumy + clumps(i,2);
         catch
@@ -12,8 +15,10 @@ locationSumy = 0;
         end
   
     end
+    %Find the average clump location
     averageLocation(1,1) = locationSumx/length(clumps);
     averageLocation(1,2) = locationSumy/length(clumps);
+    %Return position of [5,5] if clumps list is empty
     if (isnan(averageLocation(1)) && isnan(averageLocation(2)))
         averageLocation(:) = [5,5];
     end
